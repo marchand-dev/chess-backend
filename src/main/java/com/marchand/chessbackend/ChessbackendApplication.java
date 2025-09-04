d525221a913966dd455d96662ef9f8cd471035a8
marchand-dev <alexmarchandrs@gmail.com>
2025-09-04 03:31:21 -0300

    initial commit

---
+++ src/main/java/com/marchand/chessbackend/ChessbackendApplication.java
@@ -0,0 +1,13 @@
+package com.marchand.chessbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marchand/chessbackend/ChessbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchand.chessbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
